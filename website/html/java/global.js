0f85e2aa78842f223c8d8bf0d70ade88b68a419d
kimm625 <70741883+kimm625@users.noreply.github.com>
2020-12-16 01:09:33 +0900

    java

---
+++ website/html/java/global.js
@@ -0,0 +1,7 @@
+
+    $(window).load(function() {
+      $('#liquid').display = "";  
+      $('#liquid').fadeIn('slow', function() {
+           // 애니메이션 끝난 뒤 실행할 코드 넣기
+      });
+    });
---
+++ website/html/java/liquid.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+  $('#liquid').display = "";  
+  $('#liquid').fadeIn('slow', function() {
+       // 애니메이션 끝난 뒤 실행할 코드 넣기
+  });
+});
