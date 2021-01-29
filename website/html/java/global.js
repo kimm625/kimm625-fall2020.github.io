58bc40735f5e67b8cb296308237c891d5b8c090a
kimm625 <70741883+kimm625@users.noreply.github.com>
2021-01-29 20:13:22 +0900

    new page

--- website/html/java/global.js
+++
@@ -1,9 +0,0 @@
-
-    var text = document.getElementById("text");
-    
-    $(window).load(function() {
-      $('#liquid').display = "";  
-      $('#liquid').fadeIn('slow', function() {
-           // 애니메이션 끝난 뒤 실행할 코드 넣기
-      });
-    });
--- website/html/java/liquid.js
+++
@@ -1,8 +0,0 @@
-var text = document.getElementsByClassName("text");
-
-$(window).load(function() {
-  $('.item').display = "";  
-  $('.item').fadeIn('slow', function() {
-       // 애니메이션 끝난 뒤 실행할 코드 넣기
-  });
-});
